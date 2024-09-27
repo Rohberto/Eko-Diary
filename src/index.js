09f30cf9ef97d6c015ebcc8a4bfdec414434e7be
Robert <oluwaseunrobert44@gmail.com>
2024-09-27 11:49:53 +0100

    Polyfill

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+import "./polyfill";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import {Provider} from 'react-redux';
 import store from './Store';
